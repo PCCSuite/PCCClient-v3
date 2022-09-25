0cbe908664e878c602af4227030fbcc0e0f6fcbf
ozraru <34647405+ozraru@users.noreply.github.com>
2022-09-25 19:08:34 +0900

    Fix error

--- pccclient/lib/utils/plugins/command.dart
+++ pccclient/lib/utils/plugins/command.dart
@@ -5,20 +5,20 @@ import 'dart:convert';
 import 'package:pccclient/utils/plugins/start.dart';
 
 void startPluginRestore() {
-  socket!.add(json.encode({
+  socket!.write(json.encode({
     "data_type": "restore"
   }));
 }
 
 void installPlugin(String name) {
-  socket!.add(json.encode({
+  socket!.write(json.encode({
     "data_type": "install",
     "plugin": name
   }));
 }
 
 void startPluginAction(String plugin, String action) {
-  socket!.add(json.encode({
+  socket!.write(json.encode({
     "data_type": "action",
     "plugin": plugin,
     "action": action
