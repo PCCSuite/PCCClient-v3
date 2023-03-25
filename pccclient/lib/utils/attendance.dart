461fc5f210450de6d47e964e6621667af1660a79
ozraru <ozraru@raru.work>
2023-03-25 19:49:53 +0900

    Implement attendance

--- pccclient/lib/utils/attendance.dart
+++ pccclient/lib/utils/attendance.dart
@@ -1,11 +1,27 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
+import 'auth.dart';
+import 'server_info.dart';
+
 Future<void> registerAttendance() async {
-  // TODO: register attendance
-  // http.Response response = await http.get(Uri.parse(serverInfo.attendanceURL),
-  //     headers: {"Authorization": "Bearer ${await getToken()}"});
-  // if (response.statusCode != 200) {
-  //   throw Exception(
-  //       "Failed to register attendance: ${response.statusCode}, body: ${response.body}");
-  // }
+  http.Response response = await http.post(
+    Uri.parse(serverInfo.attendanceURL),
+    headers: {
+      // "Authorization": "Bearer ${await getToken()}",
+      "Content-Type": "application/json",
+    },
+    body: jsonEncode(
+      {
+        "status": "online",
+        "name": username,
+        "reg_type": "pccclient",
+      },
+    ),
+  );
+  if (response.statusCode != 200) {
+    throw Exception(
+        "Failed to register attendance: ${response.statusCode}, body: ${response.body}");
+  }
 }
