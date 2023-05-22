9c9c7dfd12ccc4cfec3c4377d288330ac839fe26
ozraru <ozraru@raru.work>
2023-05-23 01:32:28 +0900

    v3.0.1-beta.2

--- pccclient/lib/main.dart
+++ pccclient/lib/main.dart
@@ -15,7 +15,7 @@ import 'screens/plugin_manage.dart';
 import 'screens/samba.dart';
 import 'screens/settings.dart';
 
-const version = "3.0.1-beta.1";
+const version = "3.0.1-beta.2";
 
 void main() async {
   runApp(const MyApp());
